1b15bef4069c9264bfe1ea99f0ed33714fbcffd3
sahilchopra112 <sahilchopra185@gmail.com>
2025-01-09 02:58:47 +0800

    Update FavoriteRepository.java

--- src/main/java/com/rental/gamerent/repo/FavoriteRepository.java
+++ src/main/java/com/rental/gamerent/repo/FavoriteRepository.java
@@ -2,9 +2,17 @@ package com.rental.gamerent.repo;
 
 import com.rental.gamerent.model.Favorite;
 import org.springframework.data.jpa.repository.JpaRepository;
+<<<<<<< Updated upstream
 
 import java.util.List;
 
+=======
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+>>>>>>> Stashed changes
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
     List<Favorite> findByUserId(Long userId);
 
